cd785c0c2d63d6637ccc85179c8a40d94b150d9e
Mohamed Mohsen Brary <mohamedbrary@gmail.com>
2017-05-31 18:24:29 +0200

    Chatting between two users, and retrieving data from firebase

--- firebase_chat.js
+++ firebase_chat.js
@@ -1,6 +1,18 @@
 $( window ).on( "load", function() {
-	
-	// Initialize Firebase
+  
+  // Initialize Firebase
+  initFirebase();
+  
+  // Begin listening for data
+  startListening();
+
+  // Initialize our chat app
+  initChatApp();
+
+});
+
+/** Function to init Firebase **/
+initFirebase = function() {
   var config = {
     apiKey: "AIzaSyCLeU7v5DxQiOBqXDKM28D3UZZsXCVqYJY",
     authDomain: "fir-sample-chat-94755.firebaseapp.com",
@@ -9,20 +21,95 @@ $( window ).on( "load", function() {
     storageBucket: "fir-sample-chat-94755.appspot.com",
     messagingSenderId: "938343873148"
   };
-  firebase.initializeApp(config);
+  firebase.initializeApp(config);  
+}
 
-	// var myFirebase = new Firebase('https://fir-sample-chat-94755.firebaseio.com/');
-	var usernameInput = $('#username');
-	var othernameInput = $('#othername');
+/** Function to add a data listener **/
+startListening = function() {
+  firebase.database().ref().on('child_added', function(snapshot) {
+    var chatItem = snapshot.val();
+    
+    if ( chatItem.from == 'Designer' )
+      renderDesignerMsg(chatItem.msg)
+    else
+      renderDeveloperMsg(chatItem.msg)    
+  });
+}
+
+initChatApp = function() {
+  var usernameInput = $('#username');
+  var othernameInput = $('#othername');
   var textInput = $('#chat_text');
   var postButton = $('#post_button');
+  var switchUsersBtns = $('.dev-switch-button');
+
+  currentRenderFunction = renderDeveloperMsg;
+  otherRenderFunction = renderDesignerMsg;
 
   postButton.click( function() {
     var fromUser = usernameInput.text().trim();
     var toUser = othernameInput.text().trim();
     var msgText = textInput.val().trim();
-    firebase.database().ref().set(fromUser + " tells " + toUser + ": " + msgText);
-    textInput.value = "";
+    
+    firebase.database().ref().push({
+      from: fromUser,
+      to: toUser,
+      msg: msgText
+    });
+
+    textInput.val('');
+    // $("#chat_items_list").append(currentRenderFunction(msgText));
   });
 
-})
+  switchUsersBtns.click(toggleUser);
+}
+
+function toggleUser() {
+  // Switch usernames  
+  var tmp = $('#username').text();
+  $('#username').text($('#othername').text());
+  $('#othername').text(tmp);
+
+  // Switch buttons status  
+  $('.dev-switch-button').each(function( index ) {
+    if ( $(this).hasClass('disabled') )
+      $(this).removeClass('disabled');
+    else
+      $(this).addClass('disabled');
+  });
+  
+  // Switch message template also
+  var tmp = currentRenderFunction;
+  currentRenderFunction = otherRenderFunction;
+  otherRenderFunction = tmp;
+
+}
+
+function renderDesignerMsg(text){
+  return renderMsg('', 'https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg', text);
+}
+
+function renderDeveloperMsg(text){
+  return renderMsg('other-msg admin_chat', 'https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-1/c22.0.48.48/p48x48/13615419_10153592621422793_8368169177989329233_n.jpg?oh=f5ef37717a144053fcdf7e6fbd42ff02&oe=59E439CE', text);  
+}
+
+function renderMsg(className, imgSrc, text){
+  imgClass = className != '' ? 'right' : 'left';
+  timeClass = className == '' ? 'left' : 'right';
+  chatItemHtml = `
+    <li class="${className} left clearfix">
+      <span class="chat-img1 pull-${imgClass}">
+        <img alt="User Avatar" class="img-circle" src="${imgSrc}"/>
+      </span>
+      <div class="chat-body1 clearfix">
+        <p>${text}</p>
+        <div class="text-muted small chat_time pull-${timeClass}">
+          09:40PM
+        </div>
+      </div>
+    </li>
+  `;
+
+  $("#chat_items_list").append(chatItemHtml);
+  $(".chat_area").animate({ scrollTop: $('.chat_area').prop("scrollHeight")}, 300);
+}
